1ed4b98a6664ac82a323f2afbbf852eb1201b436
Matteo Ceccarello <ceccarel@dei.unipd.it>
2016-11-18 16:03:31 +0100

    Add .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((c++-mode
+  (flycheck-checker . c/c++-gcc)
+  (flycheck-gcc-language-standard . "c++11")
+  (flycheck-gcc-openmp . t)))
