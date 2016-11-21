9ab4bb9f503fd4f951bf526062fe83ae50def2a3
Matteo Ceccarello <ceccarel@dei.unipd.it>
2016-11-21 17:43:54 +0100

    Fix some bugs in the logic of sequential clustering

--- core/sequential_clustering.cpp
+++ core/sequential_clustering.cpp
@@ -1,5 +1,15 @@
 #include "sequential_clustering.hpp"
 
+size_t count_uncovered(const std::vector< SequentialClusterVertex > & vinfo) {
+  size_t cnt = 0;
+  for (const auto & v : vinfo) {
+    if (!v.is_covered()) {
+      cnt++;
+    }
+  }
+  return cnt;
+}
+
 ugraph_vertex_t pick_vertex(const ugraph_t & graph,
                             const std::vector< SequentialClusterVertex > & vinfo) {
   auto n = boost::num_vertices(graph);
@@ -21,25 +31,34 @@ std::vector< SequentialClusterVertex > sequential_cluster(const ugraph_t & graph
   std::vector< SequentialClusterVertex > vinfo(n);
   std::vector< probability_t > probabilities(n);
   probability_t p_curr = 1.0;
-  size_t uncovered = boost::num_vertices(graph);
+  size_t uncovered = n;
 
   while (p_curr > p_low) {
-    LOG_INFO("Build clustering with p_curr="<<p_curr);
+    LOG_INFO("Build clustering with p_curr=" << p_curr);
     std::fill(vinfo.begin(), vinfo.end(), SequentialClusterVertex());
+    uncovered = n;
     sampler.min_probability(graph, p_curr);
+
+    // Build the clustering
     for (size_t center_cnt = 0; center_cnt < k; center_cnt++) {
+      assert(uncovered == count_uncovered(vinfo));
       ugraph_vertex_t center = pick_vertex(graph, vinfo);
       vinfo[center].make_center(center);
+      uncovered--;
       sampler.connection_probabilities(graph, center, probabilities);
       // Cover the nodes
       for (ugraph_vertex_t i=0; i<n; i++) {
-        if (probabilities[i] >= p_curr && (!vinfo[i].is_covered() || vinfo[i].probability() <probabilities[i])) {
-          vinfo[i].cover(center, probabilities[i]);
-          uncovered--;
+        if (probabilities[i] >= p_curr) {
+          if (!vinfo[i].is_covered()) {
+            vinfo[i].cover(center, probabilities[i]);
+            uncovered--;
+          } else if (vinfo[i].probability() < probabilities[i]) {
+            vinfo[i].cover(center, probabilities[i]);
+          }
         }
       }
       
-      if (center_cnt + uncovered < k + slack) {
+      if (center_cnt + uncovered <= k + slack) {
         // Return the clustering
          for (ugraph_vertex_t i=0; i<n; i++) {
            if (!vinfo[i].is_covered()) {
@@ -49,7 +68,8 @@ std::vector< SequentialClusterVertex > sequential_cluster(const ugraph_t & graph
          return vinfo;
       }
     }
-    
+
+    LOG_INFO("Still " << uncovered << " nodes to cover");
     // update the probability
     p_curr *= rate;
   }
