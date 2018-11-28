2a51f10940c03ca523c7fef3604fbbd2e986be78
Matteo Ceccarello <ceccarel@dei.unipd.it>
2018-11-28 20:21:51 +0100

    Uncompress the dblp input prior to Docker image build
    There are some problems doing it in Docker run, and now I don't have
    time to investigate it.

--- run-docker.sh
+++ run-docker.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+pushd Reproducibility/Data/dblp
+bunzip2 -k dblp-vldb-publication.txt.bz2
+popd
+
 docker build -t ugraph-repro .
 test -d reproducibility-results || mkdir reproducibility-results
 docker run \
