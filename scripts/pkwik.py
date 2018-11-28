2f47e62e487ed78afdeba41b4caefd61ecf22459
Matteo Ceccarello <ceccarel@dei.unipd.it>
2018-11-28 13:42:12 +0100

    Update pkwik.py to work with networkx version 2

--- scripts/pkwik.py
+++ scripts/pkwik.py
@@ -4,6 +4,7 @@ import networkx as nx
 import sys
 import random
 
+assert int(nx.__version__.split('.')[0]) == 2, 'Networkx version 2 is required'
 
 def load(path):
     G = nx.read_edgelist(path, data=[('probability', float)])
@@ -13,15 +14,15 @@ def load(path):
 def pKwikCluster(graph):
     clusters = []
     # Filter edges with probability less than 0.5
-    valid_edges = [(u, v) for u, v, d in graph.edges_iter(data='probability')
+    valid_edges = [(u, v) for u, v, d in graph.edges(data='probability')
                    if d >= 0.5]
     G = nx.Graph(valid_edges)
     print('Filtered graph has',G.number_of_nodes(),
           'nodes and', G.number_of_edges(), 'edges',
           file=sys.stderr)
     while G.number_of_nodes() > 0:
-        root = random.choice(G.nodes())
-        star = list(G[root].keys())
+        root = random.choice(list(G.nodes))
+        star = list(G.neighbors(root))
         cluster = [root] + star
         clusters.append(cluster)
         G.remove_nodes_from(cluster)
